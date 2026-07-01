1b4896a21ae1bdfa2d24610fbff96025ad2561ec
taeho2344 <taeho2344@gmail.com>
2026-07-01 23:43:03 +0900

    towers of hanoi problem
    based on my memory at this afternoon

--- data_structures/02_recursion/p_05/taeho/taeho_2_5.c
+++ data_structures/02_recursion/p_05/taeho/taeho_2_5.c
@@ -1,3 +1,17 @@
-//
-// Created by MSI on 26. 7. 1..
-//
+#include <stdio.h>
+void hanoi(char,char,char,int);
+int main() {
+    int plate_count;
+    scanf("%d",&plate_count);
+    hanoi('A','B','C',plate_count);
+}
+void hanoi(char from,char aux,char to,int plate_count) {
+    if (plate_count==1) {
+        printf("from %c to %c\n",from,to);
+        return;
+    }
+    hanoi(from,to,aux,plate_count-1);
+    printf("from %c to %c\n",from,to);
+    hanoi(aux,from,to,plate_count-1);
+
+}
