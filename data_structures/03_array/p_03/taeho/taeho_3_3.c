2959b7a6af32beba910d11f94b09803ddf8cc49d
taeho2344 <taeho2344@gmail.com>
2026-07-02 18:21:53 +0900

    array 3-3 solved

--- data_structures/03_array/p_03/taeho/taeho_3_3.c
+++ data_structures/03_array/p_03/taeho/taeho_3_3.c
@@ -3,27 +3,43 @@
 int main() {
     int arr_size;
     scanf("%d",&arr_size);
-    int **height=(int**)calloc(arr_size,sizeof(int*));
-    if (height==NULL) {
+    int **arr=(int**)calloc(arr_size,sizeof(int*));
+    if (arr==NULL) {
         fprintf(stderr,"calloc failed");
         return -1;
     }
-    int **arr=height;
-    int **pointer_height=arr;
-    int n=1;
-    for (;pointer_height<arr+arr_size;pointer_height++) {
-        int *arr=(int*)calloc(arr_size,sizeof(int));
-        *pointer_height=arr;
-        if ((arr-pointer_height)%2) {
-            for (int *p=*pointer_height+n-1;p>=*pointer_height;p--) {
+    int **arrp=arr;
+    int n=1,column_cnt=0;
+    for (;arrp<arr+arr_size;arrp++,column_cnt++) {
+        *arrp=(int*)calloc(arr_size,sizeof(int));
+        if (*arrp==NULL) {
+            fprintf(stderr,"calloc failed");
+            for (int **q=arr;q<arrp;q++) {
+                free(*q);
+            }
+            free(arr);
+            return -1;
+        }
+        if (column_cnt%2) {
+            for (int *p=*arrp+arr_size-1;p>=*arrp;p--) {
                 *p=n++;
             }
         }
         else {
-            for (int *p=*pointer_height;p<*pointer_height+n;p++) {
+            for (int *p=*arrp;p<*arrp+arr_size;p++) {
                 *p=n++;
             }
         }
     }
-    for ()
+    for (arrp=arr;arrp<arr+arr_size;arrp++) {
+        for (int *p=*arrp;p<*arrp+arr_size;p++) {
+            printf(" %d",*p);
+        }
+        printf("\n");
+    }
+    for (arrp=arr;arrp<arr+arr_size;arrp++) {
+        free(*arrp);
+    }
+    free(arr);
+    return 0;
 }
