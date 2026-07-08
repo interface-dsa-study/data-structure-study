a20d0fc49547a7880a5256e08b003bf3a5142bcd
min816 <111326768+min816@users.noreply.github.com>
2026-07-08 16:35:19 +0900

    Update solution.c

---
+++ problems/leetcode/p_901/minwoo/solution.c
@@ -0,0 +1,20 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int t;
+}StockSpanner;
+
+StockSpanner* stockSpannerCreate() {
+
+}
+
+int main() {
+    int array[10000];
+    int N, i, num;
+
+    while (1) {
+        scanf("%d", &num);
+    }
+}
