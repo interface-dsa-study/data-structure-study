db30c00d299da9447c85469cf784883c5e501f36
taeho2344 <taeho2344@gmail.com>
2026-07-10 01:19:27 +0900

    stack problem 1-1

---
+++ data_structures/08_stack_1/p_01/taeho/taeho_8_1_linked_list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
+    char element;
+    struct node *next;
+}Node;
+typedef struct stack {
+    Node *top;
+    int size;
+}Stack;
+int main() {
+    Stack 
+}
