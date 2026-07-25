b1fae4f93b74b09219a2718ea4941411ab739405
taeho2344 <taeho2344@gmail.com>
2026-07-25 15:47:58 +0900

    stack 9_2

--- data_structures/09_stack_2/p_02/taeho/taeho_9_2.c
+++ data_structures/09_stack_2/p_02/taeho/taeho_9_2.c
@@ -12,3 +12,85 @@
 ///
 ///
 ///민우형:
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct node {
+    int value;
+    struct node *next;
+}Node;
+typedef struct stack {
+    Node *top;
+}Stack;
+void push(Stack *stack,int value) {
+    Node *new_node=(Node*)malloc(sizeof(Node));
+    if (new_node==NULL) {
+        fprintf(stderr,"node malloc failed");
+        exit(1);
+    }
+    new_node->value=value;
+    new_node->next=stack->top;
+    stack->top=new_node;
+}
+int pop(Stack *stack) {
+    if (stack->top==NULL) {
+        fprintf(stderr,"stack is empty");
+        return '\0';
+    }
+    Node *target=stack->top;
+    int value=target->value;
+    stack->top=target->next;
+    free(target);
+    return value;
+}
+int calculate(int left, int right, char calculator) {
+    switch (calculator) {
+        case '*':
+            return left*right;
+        case '/':
+            return left/right;
+        case '+':
+            return left+right;
+        case '-':
+            return left-right;
+    }
+}
+void string_calculate() {
+    Stack stack;
+    stack.top=NULL;
+    char string[101]={0};
+    scanf("%s",string);
+    for (int i=0;i<strlen(string);i++) {
+        if (string[i]>='0'&&string[i]<='9') {//피연산자->스택에 바로 푸시
+            push(&stack,string[i]-'0');
+        }
+        else if (string[i]=='*'||//연산자->스택에서 오른쪽, 왼쪽 피연산자 꺼내서 계산
+            string[i]=='/'||
+            string[i]=='+'||
+            string[i]=='-') {
+            if (stack.top==NULL) {
+                fprintf(stderr,"stack is empty");
+                return;
+            }//StackEmptyException
+            int right=pop(&stack);
+            if (stack.top==NULL) {
+                fprintf(stderr,"stack is empty");
+                return;
+            }//StackEmptyException
+            int left=pop(&stack);
+            push(&stack,calculate(left,right,string[i]));//연산한거 스택에 저장
+        }
+    }
+    printf("%d\n",pop(&stack));
+    if (stack.top) {
+        fprintf(stderr,"stqack is not empty");
+        exit(1);
+    }
+}
+int main() {
+    int time;
+    scanf("%d",&time);
+    for (int i=0;i<time;i++) {
+        string_calculate();
+    }
+}
