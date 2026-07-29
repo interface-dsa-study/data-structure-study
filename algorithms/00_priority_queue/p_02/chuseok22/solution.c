3b0c21a1a6fd3beaca9c5a2a4b425200d5d26abe
jinu829 <qkrwlsdn0503@naver.com>
2026-07-29 14:58:29 +0900

    Update solution.c

--- algorithms/00_priority_queue/p_02/chuseok22/solution.c
+++ algorithms/00_priority_queue/p_02/chuseok22/solution.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *value1, int *value2) {
+void swap(int *value1, int *value2)
+{
   int tmp = *value1;
   *value1 = *value2;
   *value2 = tmp;
 }
 
-int *insertionSort(int *array, int size) {
-  for (int i = 1; i < size; i++) {
-    for (int j = i; j > 0; j--) {
-      if (array[j - 1] > array[j]) {
+int *insertionSort(int *array, int size)
+{
+  for (int i = 1; i < size; i++)
+  {
+    for (int j = i; j > 0; j--)
+    {
+      if (array[j - 1] > array[j])
+      {
         swap(&array[j - 1], &array[j]);
       }
     }
@@ -19,22 +24,27 @@ int *insertionSort(int *array, int size) {
   return array;
 }
 
-void printArray(int *array, int size) {
-  for (int i = 0; i < size; i++) {
+void printArray(int *array, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     printf(" %d", array[i]);
   }
 }
 
-int main(void) {
+int main(void)
+{
   int size = 0;
   scanf("%d", &size);
 
   int *array = malloc(sizeof(*array) * size);
-  if (array == NULL) {
+  if (array == NULL)
+  {
     exit(EXIT_FAILURE);
   }
 
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++)
+  {
     scanf("%d", &array[i]);
   }
 
