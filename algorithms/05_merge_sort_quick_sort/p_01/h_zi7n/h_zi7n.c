9dce843d2d6a39a36e3b63e5b75121b593195521
h-zi7n <h_zi7n@naver.com>
2026-08-05 15:14:19 +0900

    Update h_zi7n.c

--- algorithms/05_merge_sort_quick_sort/p_01/h_zi7n/h_zi7n.c
+++ algorithms/05_merge_sort_quick_sort/p_01/h_zi7n/h_zi7n.c
@@ -4,25 +4,14 @@
 typedef int baeyul[];
 typedef int buffer[];
 
-void mergeSort(baeyul,buffer, int left, int right) {
-    for (int i = left; i < right; i++) {
-        buffer[i] = baeyul[i];
-    }
-    for (int i = left; i < right; i++) {
-        for (int j = i; j < right; j++) {
-            if (buffer[i] > buffer[j]) {
-                int tmp = buffer[i];
-                buffer[i] = buffer[j];
-                buffer[j] = tmp;
-            }
-        }
-    }
-    for (int i = left; i < right; i++) {
-        baeyul[i] = buffer[i];
-    }
+void merge(baeyul, buffer, int left, int middle, int right) {
+    int start = left;
+    int end = 
+    int
 }
 
-void merge(baeyul, buffer, int left, int middle, int right) {
+void mergeSort(baeyul,buffer, int left, int right) {
+
 
 }
 
@@ -35,5 +24,5 @@ int main() {
         scanf("%d", &integer);
         baeyul[i] = integer;
     }
-    
+
 }
