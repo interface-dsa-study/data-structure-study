2bf6f556e883bb0663241203aa481f06662cc969
h-zi7n <h_zi7n@naver.com>
2026-08-05 15:59:16 +0900

    알고리즘 5주차

--- algorithms/05_merge_sort_quick_sort/p_01/h_zi7n/h_zi7n.c
+++ algorithms/05_merge_sort_quick_sort/p_01/h_zi7n/h_zi7n.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int baeyul[];
-typedef int buffer[];
-
-void merge(baeyul, buffer, int left, int middle, int right) {
+void merge(int array[], int buffer[], int left, int middle, int right) {
     int start = left;
-    int end = 
-    int
-}
-
-void mergeSort(baeyul,buffer, int left, int right) {
-
-
-}
-
-int main() {
-    int N; scanf("%d", &N);
-    baeyul = (int*)malloc(sizeof(int)*N);
-    buffer = (int*)malooc(sizeof(int)*N);
-    int integer;
-    for (int i = 0; i < N; i++){
-        scanf("%d", &integer);
-        baeyul[i] = integer;
+    int start_two = middle + 1;
+    int buffer_start_index = left;
+    while (start == middle && start_two == right) {
+        if (array[start] < array[start_two]) {
+            buffer[buffer_start_index++] = array[start++];
+        }
+        else buffer[buffer_start_index++] = array[start_two++];
     }
+    if (start == middle) {
+        while (start_two != right) {
+            buffer[buffer_start_index++] = array[start_two++];
+        }
+    }
+    else {
+        while (start != middle) {
+            buffer[buffer_start_index++] = array[start++];
+        }
+    }
+}
 
+void mergeSort(int array[], int buffer[], int left, int right) {
+    if (right < left) { // 종료조건
+        return;
+    }
+    int middle = (left+right) / 2;
+    mergeSort(array, buffer, left, middle);
+    mergeSort(array, buffer, middle + 1, right);
+    merge(array, buffer, left, middle, right);
+}
+
+int main () {
+    int N; scanf("%d", &N);
+    int *array = malloc(sizeof(*array)*N);
+    int *buffer = malloc(sizeof (*buffer)*N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &array[i]);
+    }
+    mergeSort(array, buffer, 0, N - 1);
+    for (int i = 0; i < N; i++) {
+        array[i] = buffer[i];
+        printf(" %d", array[i]);
+    }
+    free(array); free(buffer);
 }
---
+++ algorithms/05_merge_sort_quick_sort/p_01/h_zi7n/h_zi7n_new.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int array[], int buffer[], int left, int middle, int right) {
+    int left_index = left;
+    int right_index = middle + 1;
+    int buffer_index = left;
+
+    while (left_index <= middle || right_index <= right) {
+        if (array[left_index] <= array[right_index]) {
+            buffer[buffer_index] = array[left_index];
+            left_index++;
+        } else {
+            buffer[buffer_index] = array[right_index];
+            right_index++;
+        }
+        buffer_index++;
+    }
+
+    if (left_index == middle) {
+        // 왼쪽 배열 모두 소진
+        while (right_index <= right) {
+            buffer[buffer_index] = array[right_index];
+            right_index++;
+            buffer_index++;
+        }
+    } else {
+        // 오른쪽 배열 모두 소진
+        while (left_index <= middle) {
+            buffer[buffer_index] = array[left_index];
+            left_index++;
+            buffer_index++;
+        }
+    }
+}
+
+void mergeSort(int array[], int buffer[], int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    int middle = (left + right) / 2;
+    mergeSort(array, buffer, left, middle);
+    mergeSort(array, buffer, middle + 1, right);
+    merge(array, buffer, left, middle, right);
+}
+
+int main(void) {
+    int size;
+    scanf("%d", &size);
+
+    int *array = malloc(sizeof(*array) * size);
+    if (array == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    int *buffer = malloc(sizeof(*buffer) * size);
+    if (buffer == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    mergeSort(array, buffer, 0, size - 1);
+
+    for (int i = 0; i < size; i++) {
+        array[i] = buffer[i];
+        printf(" %d", array[i]);
+    }
+
+    free(array);
+    free(buffer);
+}
