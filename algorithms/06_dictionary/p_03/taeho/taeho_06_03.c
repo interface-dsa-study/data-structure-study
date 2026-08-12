201fc8ed676d04b265a06a487a1eea544a460058
taeho2344 <taeho2344@gmail.com>
2026-08-12 15:01:47 +0900

    Create taeho_06_03.c

---
+++ algorithms/06_dictionary/p_03/taeho/taeho_06_03.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main() {
+    int a,b,n;
+    scanf("%d %d %d",&a,&b,&n);
+    char *string=malloc(sizeof(*string)*(n+1));
+
+    scanf("%s",string);
+    char *yn=string;
+    for (int i=0;a<b&&i<n;i++,yn++) {
+        int m=(a+b)/2;
+        if (*yn=='Y') a=m+1;
+        else if (*yn=='N') b=m;
+        else {
+            printf("wrong char");
+            break;
+        }
+    }
+    printf("%d",a);
+    return 0;
+}
