6e28dbd1d7211125204e760c5cbfc006690c9b45
Chuseok22 <133375485+Chuseok22@users.noreply.github.com>
2026-09-14 10:57:22 +0900

    feat(알고리즘 우현수): 1주차 1번

--- algorithms/2026_2_chuseok22/01_data_structure_review/p_01/solution.c
+++ algorithms/2026_2_chuseok22/01_data_structure_review/p_01/solution.c
@@ -13,6 +13,7 @@ typedef struct List {
 } List;
 
 Node *createNode(char element) {
+  // 새로운 노드 동적할당
   Node *node = malloc(sizeof(*node));
   if (node == NULL) {
     exit(EXIT_FAILURE);
@@ -24,10 +25,13 @@ Node *createNode(char element) {
 }
 
 List *createList(void) {
+  // 새로운 리스트 동적할당
   List *list = malloc(sizeof(*list));
   if (list == NULL) {
     exit(EXIT_FAILURE);
   }
+
+  // header, trailer 초기화
   list->header = createNode('\0');
   list->trailer = createNode('\0');
 
@@ -40,6 +44,8 @@ List *createList(void) {
 
 Node *getNode(List *list, int rank) {
   Node *current_node = list->header;
+
+  // 리스트에서 입력 rank 만큼 뒤로 이동
   for (int index = 0; index < rank; index++) {
     current_node = current_node->next;
   }
@@ -48,6 +54,8 @@ Node *getNode(List *list, int rank) {
 
 void get(List *list, int rank) {
   Node *target_node = getNode(list, rank);
+
+  // 잘못된 rank 입력 시
   if (target_node == list->header || target_node == list->trailer) {
     printf("invalid position\n");
     return;
@@ -56,14 +64,18 @@ void get(List *list, int rank) {
 }
 
 void add(List *list, int rank, char element) {
+  // 입력 rank 가 잘못된 경우
   if (rank > list->size + 1) {
     printf("invalid position\n");
     return;
   }
-  Node *new_node = createNode(element);
-  Node *target_node = getNode(list, rank);
 
+  Node *new_node = createNode(element); // 새로운 노드 생성
+  Node *target_node = getNode(list, rank); // 현 위치 노드 반환
+
+  // 가장 앞에 삽입하는경우
   if (rank == 1) {
+    // 이중 연결 리스트 노드 연결
     new_node->prev = list->header;
     new_node->next = target_node;
 
@@ -73,27 +85,33 @@ void add(List *list, int rank, char element) {
     list->size++;
     return;
   }
+  // 이중 연결 리스트 노드 언결
   new_node->prev = target_node->prev;
   new_node->next = target_node;
 
   target_node->prev->next = new_node;
   target_node->prev = new_node;
 
+  // 리스트 size 증가
   list->size++;
 }
 
 void deleteNode(List *list, int rank) {
+  // 잘못된 rank 입력 시
   if (rank > list->size) {
     printf("invalid position\n");
     return;
   }
 
+  // target_node 연결 제거
   Node *target_node = getNode(list, rank);
   Node *previous_node = target_node->prev;
   Node *next_node = target_node->next;
 
   previous_node->next = next_node;
   next_node->prev = previous_node;
+
+  // target_node 메모리 해제
   free(target_node);
   list->size--;
 }
@@ -104,6 +122,8 @@ void print(List *list) {
   }
 
   Node *current_node = list->header->next;
+
+  // 리스트 순회 및 출력
   while (current_node != NULL && current_node != list->trailer) {
     printf("%c", current_node->element);
     current_node = current_node->next;
@@ -116,6 +136,8 @@ void destroyList(List *list) {
     return;
   }
   Node *current_node = list->header;
+
+  // 리스트 순회 동적할당 제거
   while (current_node != NULL) {
     Node *next_node = current_node->next;
     free(current_node);
