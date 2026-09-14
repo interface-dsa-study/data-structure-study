d785a485e3de6e8993884273e406598133edcac6
Chuseok22 <133375485+Chuseok22@users.noreply.github.com>
2026-09-14 10:10:45 +0900

    feat(알고리즘 우현수): 1주차 2번

---
+++ algorithms/2026_2_chuseok22/01_data_structure_review/p_02/solution.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct Node {
+  int data;
+  struct Node *left;
+  struct Node *right;
+} Node;
+
+typedef struct Tree {
+  Node *root;
+} Tree;
+
+Node *createNode(int data) {
+  Node *node = malloc(sizeof(*node));
+  if (node == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return node;
+}
+
+Node *buildNode(void) {
+  int x, y, z;
+
+  // x y z 입력
+  scanf("%d %d %d", &x, &y, &z);
+
+  // x 노드 생성
+  Node *node = createNode(x);
+
+  // y != 0 이면 왼쪽 서브트리 생성
+  if (y != 0) {
+    node->left = buildNode();
+  }
+
+  // z != 0 이면 오른쪽 서브트리 생성
+  if (z != 0) {
+    node->right = buildNode();
+  }
+
+  // 현재 노드 반환
+  return node;
+}
+
+void printNode(Node *root, char command_string[]) {
+  printf(" %d", root->data);
+
+  Node *current_node = root;
+  for (int i = 0; i < strlen(command_string); i++) {
+    if (command_string[i] == 'L') {
+      current_node = current_node->left;
+    } else if (command_string[i] == 'R') {
+      current_node = current_node->right;
+    }
+    printf(" %d", current_node->data);
+  }
+  printf("\n");
+}
+
+void destroyTree(Node *root) {
+  if (root == NULL) {
+    return;
+  }
+  destroyTree(root->left);
+  destroyTree(root->right);
+  free(root);
+}
+
+int main(void) {
+  int node_count, search_count;
+  char command_string[101];
+  scanf("%d", &node_count);
+
+  Tree tree;
+  tree.root = buildNode();
+
+  scanf("%d", &search_count);
+
+  for (int i = 0; i < search_count; i++) {
+    scanf("%s", command_string);
+    printNode(tree.root, command_string);
+  }
+  destroyTree(tree.root);
+}
